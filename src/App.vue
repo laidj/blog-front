ae1c6011f230b62fbb3ad580567b3b52032f68bf
Jakob Laid <jakob.laid@tptlive.ee>
2024-02-20 12:56:31 +0200

    add item list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
+import { ref } from 'vue';
 
+
+let message = ref("Hello neegro");
+let items = ref(['sai', 'suits', 'gin']);
+function add(){
+  if(message.value.trim() !== ''){
+    items.value.push(message.value.trim());
+  }
+  message.value = '';
+}
 </script>
 
 <template>
-
+<button @click="add" >Click me bitch</button>
+<input type="text" v-model="message" @keydown.enter="add">
+<ul>
+  <li v-for="item in items">{{ item }}</li>
+</ul>
 </template>
 
 
